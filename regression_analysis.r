30f2922edf5b4f2b545fd3d501e3c44d5c409597
carycorreia <carycorreia@mac.com>
2014-11-19 21:45:06 -0500

    Update as of Wed November 19, 2014
    Finished the assignment.  At least for round 1.  Managed to complete it
    in 2 pages with a 3 page appendix.

--- regression_analysis.r
+++ regression_analysis.r
@@ -2,8 +2,11 @@
 #
 # Load and explore database mtcars
 data(mtcars)
-mtcars
-#
+name(mtcars)
+cor(mtcars$mpg, mtcars)
+
+# Explore cross correlation of all variables
+cor(mtcars)
 # Draw pairwise graphs
 pairs(mtcars)
 
@@ -15,30 +18,50 @@ mtcars$vs<-factor(mtcars$vs)
 mtcars$cyl<-factor(mtcars$cyl)
 
 #2 Initial analysis for am variable
-plot(mpg~am, mtcars)
-# add in t-test
+p.mpg.am<-t.test(mpg~am, mtcars)
+plot(mpg~am, main="MPG vs Transmission Boxplot", mtcars)
+# add legend with P-value result
+temp<-legend("topleft", legend=c("P-Value", round(as.numeric(p.mpg.am[3]), 6)), 
+             text.width=strwidth("Automatic"),
+             title=("t.test Results"))
+
+deltaAm<-(median(mpg[mtcars$am==1]))-(median(mpg[mtcars$am==0])); deltaAm
+deltaAm2<-(mean(mpg[mtcars$am==1]))-(mean(mpg[mtcars$am==0])); deltaAm2
 
 #3 explore am with other variables
 step.fit<-step(lm(mpg ~ ., mtcars), trace=0, steps=20000)
 summary(step.fit)
 par(mfrow=c(2,2));plot(step.fit)
+mtcars$pred.step.fit<-predict(step.fit)
 # Rsq 0.8659 RsqAdj 0.8401
 # model = cyl, hp, wt and am: note am1 not significant nor cyl 18
 
-step.fit.wI<-lm(mpg ~ wt:am + cyl:hp+ +am:hp + cyl:wt + am + wt + cyl +hp, mtcars)
+# Set Intercept so that it's meaninful
+mtcars$wtFix<-with(mtcars, wt-min(wt))
+mtcars$hpFix<-with(mtcars, hp-min(hp))
+
+step.fit.wI<-lm(mpg ~ wtFix:am + cyl:hpFix+ +am:hpFix + cyl:wtFix + am + wtFix + cyl +hpFix, mtcars)
 summary(step.fit.wI)
 par(mfrow=c(2,2));plot(step.fit.wI)
+mtcars$pred.step.fit.wI<-predict(step.fit.wI)
+
 # Rsq 0.9074, RsqAdj 0.8564
 # significant factors: wt:am1, cyl:hp --> plus most other variables not significant
 
-best.fit.wI<-lm(mpg ~ wt:am + cyl:hp +  am + wt + cyl +hp, mtcars)
+best.fit.wI<-lm(mpg ~ wtFix:am + cyl:hpFix +  am + wtFix + cyl + hpFix, mtcars)
 summary(best.fit.wI)
 par(mfrow=c(2,2));plot(best.fit.wI)
 plot(predict(best.fit.wI), resid(best.fit.wI), pch=19)
+mtcars$pred.best.fit.wI<-predict(best.fit.wI)
+
 # Rsq 0.9036, RsqAdj 0.8701
 # significant factors: am1, wt, wt:am
 
 # basic plots
+par(mfrow=c(1,3))
+plot(mtcars$pred.step.fit, mtcars$mpg)
+plot(mtcars$pred.step.fit.wI, mtcars$mpg)
+plot(mtcars$pred.best.fit.wI, mtcars$mpg)
 
 # mpg ~ wt + am--> running two separate regressions:
 regr1<- lm(mpg[am==0] ~ wt[am==0], mtcars)
@@ -84,5 +107,8 @@ with(mtcars, {
               interaction.plot(vs, cyl, mpg, fixed = TRUE, col = 2:3, leg.bty = "o")
 })
 
-
+# Nested Model Test
+anova(step.fit, best.fit.wI)
+anova(step.fit, step.fit.wI)
+anova(best.fit.wI, step.fit.wI)
 
